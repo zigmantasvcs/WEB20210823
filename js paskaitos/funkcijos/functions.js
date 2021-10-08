584ead5ef9a638d4cb84ebdd1849646e4a893bec
Zigmantas <zigmantas@mrorangetech.se>
2021-10-08 20:21:48 +0300

    funkcijos pridetos

---
+++ js paskaitos/funkcijos/functions.js
@@ -0,0 +1,27 @@
+function removeItemFromArray(array, item){
+    
+    let arrayWithoutItem = [];
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+        if(element != item){
+            arrayWithoutItem.push(element);
+        }
+    }
+
+    return arrayWithoutItem;
+}
+
+// container turi buti html elementas
+function displayItems(array, container){
+
+    let innerItems = "";
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+        innerItems += "<div>"+ element +"</div>";
+    }
+
+    container.innerHtml = innerItems;
+}
